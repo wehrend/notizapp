332737bfd780eb9dfdddbd8c0c0e6e4d30bfbfe9
Sven Wehrend <swehrend@ant-neuro.com>
2026-04-01 17:14:37 +0200

    implement rendering of cards

---
+++ index.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", renderNotes);
+
+const notes = [
+  {
+    id: 1,
+    title: "Notiz 1",
+    content: "Dies ist eine Beispiel-Notiz",
+    lastUpdated: 7558626633,
+  },
+  {
+    id: 2,
+    title: "Notiz 2",
+    content: "Dies ist eine Beispiel-Notiz",
+    lastUpdated: 1157558626633,
+  },
+  {
+    id: 3,
+    title: "notiz 3",
+    content: "Dies ist eine Beispiel-Notiz",
+    lastUpdated: 857558626633,
+  },
+  {
+    id: 4,
+    title: "notiz 4",
+    content: "Dies ist eine Beispiel-Notiz",
+    lastUpdated: 457558626633,
+  },
+];
+
+function renderNotes() {
+  console.log("calling renderNotes();");
+
+  notes.forEach((note) => {
+    renderNoteEntry(note);
+  });
+}
+
+function renderNoteEntry(note) {
+  var noteEntryDiv = document.createElement("div");
+  noteEntryDiv.classList.add("note-entry");
+
+  var noteTitleDiv = document.createElement("div");
+  noteTitleDiv.textContent = note.title;
+  noteTitleDiv.classList.add("note-title");
+  noteEntryDiv.appendChild(noteTitleDiv);
+
+  var noteContentDiv = document.createElement("div");
+  noteContentDiv.textContent = note.content;
+  noteContentDiv.classList.add("note-content");
+  noteEntryDiv.appendChild(noteContentDiv);
+
+  var noteTimestampDiv = document.createElement("div");
+  noteTimestampDiv.textContent = note.lastUpdated;
+  noteTimestampDiv.classList.add("note-date");
+  noteEntryDiv.appendChild(noteTimestampDiv);
+
+  console.log(noteEntryDiv);
+  if (noteEntryDiv) {
+    document.getElementById("notes-list").appendChild(noteEntryDiv);
+  }
+}
